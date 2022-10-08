86f53610f95683193c26c595a4a516eacd07ccbd
NatalieTesam <nataliekay1023@gmail.com>
2022-10-07 20:27:36 -0600

    Co-authored-by: chanyoung1999 <chanyoung1999@users.noreply.github.com>

--- card.cs
+++ card.cs
@@ -3,19 +3,15 @@ using System;
 
 namespace card_game
 
-
-{
-    // a card has a value from 1-13 randomized
+{   // a card has a value from 1-13 randomized
     // the responsibility of card is to keep track of its currently rolled value
     // and if it is higher or lower than the next one
     public class Card
     {
         public int value = 0;
-        public int points = 0;
 
-        public Card()
-        {
-        }
+
+    
 
 
         // Generates a new card value from 1-13
@@ -26,4 +22,5 @@ namespace card_game
         }
 
     }
+    
 }
--- director.cs
+++ director.cs
@@ -3,6 +3,9 @@
     public class Director
     {
         public bool _isPlaying = true;
+        public int _yourScore = 300;
+        public string _relation = "";
+        public Card newCard = new Card();
 
         public void StartGame()
         {
@@ -12,21 +15,60 @@
                 DoUpdates();
                 DoOutputs();
             }
+        // The end message
+        System.Console.WriteLine("--Game Over--");
         }
 
         public void GetInputs()
         {
+            // Draw the first card and tell the user the value
             Card theCard = new Card();
             theCard.DrawCard();
-            System.Console.WriteLine(theCard.value);
+            // displays the current card's value
+            System.Console.WriteLine($"\nThe card is: {theCard.value}");
+
+            // Draw a second card and compare the value and save the relation
+            newCard.DrawCard();
+
+            if(newCard.value > theCard.value)
+            {
+                _relation = "h";
+            }
+            else if(newCard.value < theCard.value)
+            {
+                _relation = "l";
+            }
+            
         }
 
         public void DoUpdates()
         {
-            
+            // Their guess if it is higher or lower (h/l)
+            System.Console.WriteLine("Is the next card higher or lower?(h/l):");
+            string answer = System.Console.ReadLine();
+
+            //updates the score based on what the relation from the inputs was
+            if(answer == _relation) 
+            {
+                _yourScore = _yourScore + 100;
+            }
+            else if(answer != _relation)
+            {
+                _yourScore = _yourScore - 75;
+            }
+            // displays the next card's value
+            System.Console.WriteLine($"Next card was: {newCard.value}");
+
         }
 
+
         public void DoOutputs()
+        {
+            // tells you the current score after guessing
+            System.Console.WriteLine($"Your score is: {_yourScore}");
+
+        // checks to see if you can keep playing if your score is above 0 you can choose if it is below you end
+        if (_yourScore > 0)
         {
             System.Console.WriteLine("Keep playing? ");
             string answer = System.Console.ReadLine();
@@ -34,6 +76,11 @@
             {
                 _isPlaying = false;
             }
+        }
+        else if(_yourScore <= 0)
+        {
+            _isPlaying = false;
+        }
         }
 
     }
